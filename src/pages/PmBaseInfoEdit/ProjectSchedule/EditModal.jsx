be16310b88a1aa8ec158698f33f75f21877473a7
Lem <932929950@qq.com>
2022-09-07 11:03:58 +0800

    维护附件id

--- src/pages/PmBaseInfoEdit/ProjectSchedule/EditModal.jsx
+++ src/pages/PmBaseInfoEdit/ProjectSchedule/EditModal.jsx
@@ -46,8 +46,7 @@ class FormModal extends PureComponent {
     });
   };
 
-  SaveUpload = (flowCallBack = this.defaultCallBack) => {
-    console.log(this.props)
+  SaveUpload = () => {
     const { dispatch,editData, fileType } = this.props;
     const dataReplace = Object.assign({},editData)
     // const { isValid,data } = this.requestHeadRef.getHeaderData();
@@ -58,18 +57,82 @@ class FormModal extends PureComponent {
         const status = this.attachmentRef.getAttachmentStatus();
         const { fileList, ready } = status;
         if (!ready) {
-          flowCallBack({
-            success: false,
-            message: '附件正在上传中，请等待上传完成后操作，否则会导致附件丢失',
-          });
+          message.warning('附件正在上传中，请等待上传完成后操作，否则会导致附件丢失');
           return;
         }
         if (fileList && fileList.length > 0) {
+          if(fileList.length > 1){
+            message.warning('该节点只允许上传一个附件，请删除原附件后上传');
+            return;
+          }
           fileList.forEach(item => {
             if (item.id && !docIdList.includes(item.id)) {
               docIdList.push(item.id);
+              switch(fileType){
+                // 维护附件查找Id
+                case 'RequireDoc': this.props.editData.requireDocId = item.id
+                  break;
+                case 'AcceptStandardDoc': this.props.editData.acceptStandardDocId = item.id
+                  break;
+                case 'StartReportDoc': this.props.editData.acceptStandardDocId = item.id
+                  break;
+                case 'UserRequireDoc': this.props.editData.userRequireDocId = item.id
+                  break;
+                case 'DesignerDoc': this.props.editData.designerDocId = item.id
+                  break;
+                case 'CropDoc': this.props.editData.cropDocId = item.id
+                  break;
+                case 'TestExampleDoc': this.props.editData.testExampleDocId = item.id
+                  break;
+                case 'TestReportDoc': this.props.editData.testReportDocId = item.id
+                  break;
+                case 'SopDoc': this.props.editData.sopDocId = item.id
+                  break;
+                case 'QuestionListDoc': this.props.editData.questionListDocId = item.id
+                  break;
+                case 'CheckListDoc': this.props.editData.checkListDocId = item.id
+                  break;
+                case 'CaseCloseReportDoc': this.props.editData.caseCloseReportDocId = item.id
+                  break;
+                case 'SatisfactionSurveyDoc': this.props.editData.satisfactionSurveyDocId = item.id
+                  break;
+                case 'PageCheckDoc': this.props.editData.pageCheckDocId = item.id
+                  break;
+              }
             }
           });
+        }else if(fileList.length === 0){
+          // 维护附件查找Id
+          switch(fileType){
+            case 'RequireDoc': this.props.editData.requireDocId = ''
+              break;
+            case 'AcceptStandardDoc': this.props.editData.acceptStandardDocId = ''
+              break;
+            case 'StartReportDoc': this.props.editData.acceptStandardDocId = ''
+              break;
+            case 'UserRequireDoc': this.props.editData.userRequireDocId = ''
+              break;
+            case 'DesignerDoc': this.props.editData.designerDocId = ''
+              break;
+            case 'CropDoc': this.props.editData.cropDocId = ''
+              break;
+            case 'TestExampleDoc': this.props.editData.testExampleDocId = ''
+              break;
+            case 'TestReportDoc': this.props.editData.testReportDocId = ''
+              break;
+            case 'SopDoc': this.props.editData.sopDocId = ''
+              break;
+            case 'QuestionListDoc': this.props.editData.questionListDocId = ''
+              break;
+            case 'CheckListDoc': this.props.editData.checkListDocId = ''
+              break;
+            case 'CaseCloseReportDoc': this.props.editData.caseCloseReportDocId = ''
+              break;
+            case 'SatisfactionSurveyDoc': this.props.editData.satisfactionSurveyDocId = ''
+              break;
+            case 'PageCheckDoc': this.props.editData.pageCheckDocId = ''
+              break;
+          }
         }
       }
       Object.assign(dataReplace, { attachmentIdList: docIdList });
@@ -98,12 +161,6 @@ class FormModal extends PureComponent {
       });
   };
 
-  defaultCallBack = res => {
-    if (!res.success) {
-      message.warning(res.message);
-    }
-  };
-
   render() {
     const { form, editData, onClose, saving, visible, loading = false, attId } = this.props;
     // const title = editData ? '编辑' : '新增';
@@ -117,6 +174,7 @@ class FormModal extends PureComponent {
       customBatchDownloadFileName: true,
       onAttachmentRef: ref => (this.attachmentRef = ref),
       entityId: get(dataReplace, 'id'),
+      maxUploadNum: 1,
       // fileList: fileList,
       // itemFieldExtra: fileList => {
       //   console.log(fileList);
