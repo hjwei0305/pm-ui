141d3531da191ec2002b02c15fe12bb4d7d02a4f
Lem <932929950@qq.com>
2022-10-17 14:53:52 +0800

    终止单据

--- src/pages/TodolistDetails/approve/ApproveDetail.jsx
+++ src/pages/TodolistDetails/approve/ApproveDetail.jsx
@@ -80,7 +80,7 @@ class ApproveDetail extends PureComponent {
   };
 
   beforeSubmit = (params) => {
-    if(params.actionType == 'turn'){
+    if(params.actionType === 'turn' || params.actionType === 'end'){
       return new Promise(resolve => {
         resolve({success: true, message: '处理中,请稍后再试' });
       });
