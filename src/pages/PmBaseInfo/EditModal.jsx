e9a6cda9c69ab4bf31fe7fa8fe075257a62e2c4b
zhangxiaotong1@donlim.com <zhangxiaotong1@donlim.com>
2023-03-15 15:35:31 +0800

    init

--- src/pages/PmBaseInfo/EditModal.jsx
+++ src/pages/PmBaseInfo/EditModal.jsx
@@ -32,7 +32,7 @@ class FormModal extends PureComponent {
     const { form, onClose, sync, visible, editData, disable } = this.props;
     const { getFieldDecorator } = form;
     let title = editData.name;
-    let url = `https://seiprod.donlim.com/#/pm-ui/pm/PmBaseInfoEdit?disable=` + disable+ "&id=" + editData.id;
+    let url = `https://sei.donlim.com/#/pm-ui/pm/PmBaseInfoEdit?disable=` + disable+ "&id=" + editData.id;
 
     return (
       <ExtModal
